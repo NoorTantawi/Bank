b327819e4fd87b20b83a459c785aacb27a69bdfb
NoorTantawi <altntawyn@gmail.com>
2025-06-20 14:57:03 +0300

    Loan

---
+++ Hope.infrastructure/DTO/LoanDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hope.infrastructure.DTO
+{
+    public class LoanDTO
+    {
+        public int LoanId { get; set; }
+        public int LoanTypeId { get; set; }
+        public int ClientId { get; set; }
+        public decimal LoanAmount { get; set; }
+        public int LoanPeriod { get; set; }
+        public decimal LoanSattelmentAmount { get; set; }
+        public decimal TotalAmountwithInterest { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal InterestRate { get; set; }
+        public decimal TaxValue { get; set; }
+        public string Currency { get; set; }
+    }
+}
